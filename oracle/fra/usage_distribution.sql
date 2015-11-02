6465fdfd4d666b41fadcfb982a6abef0c62ad96d
Roberto Reale <roberto.reale82@gmail.com>
2015-11-02 21:19:57 +0100

    Review submodule fra/usage_distribution.sql.

--- oracle/fra/usage_distribution.sql
+++ oracle/fra/usage_distribution.sql
@@ -1,20 +1,46 @@
--- 
--- Distribution of usage in recovery destination
--- 
--- Tested on: 10g, 11g
+--------------------------------------------------------------------------------
 --
+--  The SQL Diaries
+-- 
+--  Philum:    Oracle
+--  Module:    fra
+--  Submodule: fra_usage_distr
+--  Purpose:   distribution of usage in recovery destinations
+--  Tested:    10g, 11g
+--
+--  Copyright (c) 2014-5 Roberto Reale
+--  
+--  Permission is hereby granted, free of charge, to any person obtaining a
+--  copy of this software and associated documentation files (the "Software"),
+--  to deal in the Software without restriction, including without limitation
+--  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+--  and/or sell copies of the Software, and to permit persons to whom the
+--  Software is furnished to do so, subject to the following conditions:
+--  
+--  The above copyright notice and this permission notice shall be included in
+--  all copies or substantial portions of the Software.
+--  
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+--  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+--  DEALINGS IN THE SOFTWARE.
+-- 
+--------------------------------------------------------------------------------
 
--- Short info about ARCH
-archive log list;
--- natural number
-set line 200
-col name format a50
 
-select name, SPACE_LIMIT/1024/1024 as "Space Limit MBs", SPACE_USED/1024/1024 as "Space Used MBs", 
-SPACE_RECLAIMABLE/1024/1024 as "Space Usable MBs", NUMBER_OF_FILES as Files 
-from v$recovery_file_dest;
+col name format a10
 
--- Percentage
 
-set line 200
-select * from V_$RECOVERY_AREA_USAGE;
+SELECT
+	name,
+    space_limit/1024/1024        AS "Space Limit MBs",
+    space_used/1024/1024         AS "Space Used MBs", 
+    space_reclaimable/1024/1024  AS "Space Usable MBs",
+    number_of_files              AS files
+FROM
+    v$recovery_file_dest;
+
+--  ex: ts=4 sw=4 et filetype=sql
