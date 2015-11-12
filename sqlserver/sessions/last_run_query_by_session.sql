a9922479bd61d78ff4758d0859a0c55f8a4d6978
Roberto Reale <roberto.reale82@gmail.com>
2015-11-12 16:24:51 +0100

    Reviewed submodule sqlserver/sessions/last_run_query_by_session.sql.

--- sqlserver/sessions/last_run_query_by_session.sql
+++ sqlserver/sessions/last_run_query_by_session.sql
@@ -1,12 +1,52 @@
+--------------------------------------------------------------------------------
 --
---  Last Run Queries by Session
---  Tested on:
+--  The SQL Diaries
+-- 
+--  Philum:    SQL Server
+--  Module:    sessions
+--  Submodule: sess_last_queries
+--  Purpose:   last executed queries by session
+--  Tested:    2012
+--
+--  Copyright (c) 2014-5 Roberto Reale
+--  
+--  Permission is hereby granted, free of charge, to any person obtaining a
+--  copy of this software and associated documentation files (the "Software"),
+--  to deal in the Software without restriction, including without limitation
+--  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+--  and/or sell copies of the Software, and to permit persons to whom the
+--  Software is furnished to do so, subject to the following conditions:
+--  
+--  The above copyright notice and this permission notice shall be included in
+--  all copies or substantial portions of the Software.
+--  
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+--  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+--  DEALINGS IN THE SOFTWARE.
+-- 
+--------------------------------------------------------------------------------
 
 
-SELECT conn.session_id, sson.host_name, sson.login_name, 
-	sqltxt.text, sson.login_time,  sson.status
-FROM sys.dm_exec_connections conn
-INNER JOIN sys.dm_exec_sessions sson 
-ON conn.session_id = sson.session_id
-CROSS APPLY sys.dm_exec_sql_text(most_recent_sql_handle) AS sqltxt
-ORDER BY conn.session_id
+USE master
+GO
+
+SELECT
+    conn.session_id    [Session ID],
+    sql.text           [Query Text],
+    sess.login_name    [Login Name],
+    sess.login_time    [Login Time],
+    sess.status        [Session Status]
+FROM
+    sys.dm_exec_connections conn
+INNER JOIN
+    sys.dm_exec_sessions    sess 
+ON
+    conn.session_id = sess.session_id
+CROSS APPLY
+    sys.dm_exec_sql_text(most_recent_sql_handle) sql;
+
+--  ex: ts=4 sw=4 et filetype=sql
