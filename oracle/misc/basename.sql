d4f470e41a237e7f376ac9340c88d2c20d3b6d89
Roberto Reale <roberto.reale82@gmail.com>
2015-11-16 08:06:31 +0100

    Removed file oracle/misc/basename.sql.

--- oracle/misc/basename.sql
+++
@@ -1,7 +0,0 @@
---
---  Emulate GNU basename
---  See also http://www.pythian.com/blog/gnu-basename-in-plsql/
---
-
-
-select SUBSTR('&full_path', INSTR('&full_path', '&separator', -1) + 1) from dual;
