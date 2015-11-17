d5865805f334949f12e7fcfe225bf7583d76512e
Roberto Reale <roberto.reale82@gmail.com>
2015-11-17 04:11:34 +0100

    Added submodule oracle/awr/awr_control_info.sql.

---
+++ oracle/awr/awr_control_info.sql
@@ -0,0 +1,43 @@
+--------------------------------------------------------------------------------
+--
+--  The SQL Diaries
+-- 
+--  Philum:    Oracle
+--  Module:    awr
+--  Submodule: awr_control_info
+--  Purpose:   displays the control information for the Workload Repository
+--  Tested:    10g, 11g
+--
+--  Copyright (c) 2014-5 Roberto Reale
+--  
+--  Permission is hereby granted, free of charge, to any person obtaining a
+--  copy of this software and associated documentation files (the "Software"),
+--  to deal in the Software without restriction, including without limitation
+--  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+--  and/or sell copies of the Software, and to permit persons to whom the
+--  Software is furnished to do so, subject to the following conditions:
+--  
+--  The above copyright notice and this permission notice shall be included in
+--  all copies or substantial portions of the Software.
+--  
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+--  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+--  DEALINGS IN THE SOFTWARE.
+-- 
+--------------------------------------------------------------------------------
+
+CLEAR COLUMN
+
+COL dbid          FORMAT 999999999999999
+COL snap_interval FORMAT a20
+COL retention     FORMAT a20
+COL topnsql       FORMAT a20
+
+
+SELECT * FROM dba_hist_wr_control;
+
+--  ex: ts=4 sw=4 et filetype=sql
