2ce96f9728cea93cdfe368c14d2e6c890195e5cf
Roberto Reale <roberto.reale82@gmail.com>
2015-11-18 04:48:02 +0100

    Added submodule oracle/audit/aud_count_last_hour.sql.

---
+++ oracle/audit/aud_count_last_hour.sql
@@ -0,0 +1,46 @@
+--------------------------------------------------------------------------------
+--
+--  The SQL Diaries
+-- 
+--  Philum:    Oracle
+--  Module:    audit
+--  Submodule: aud_count_last_hour
+--  Purpose:   count audit records for the last hour
+--  Tested:    10g, 11g
+--
+--  Copyright (c) 2014-5 Roberto Reale
+--  
+--  Permission is hereby granted, free of charge, to any person obtaining a
+--  copy of this software and associated documentation files (the "Software"),
+--  to deal in the Software without restriction, including without limitation
+--  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+--  and/or sell copies of the Software, and to permit persons to whom the
+--  Software is furnished to do so, subject to the following conditions:
+--  
+--  The above copyright notice and this permission notice shall be included in
+--  all copies or substantial portions of the Software.
+--  
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+--  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+--  DEALINGS IN THE SOFTWARE.
+-- 
+--------------------------------------------------------------------------------
+
+
+CLEAR COLUMN
+COL count_last_hour HEADING "Audit Records for the Last Hour"
+
+
+SELECT
+    COUNT(*)  AS count_last_hour
+FROM
+    sys.aud$
+WHERE
+    CAST((FROM_TZ(ntimestamp#, '00:00') AT local) AS date)
+        < SYSDATE - INTERVAL '1' HOUR;
+
+--  ex: ts=4 sw=4 et filetype=sql
