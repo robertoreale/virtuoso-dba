42f68de4bcdca5b3f3c5abe26cc9775a6f608f89
Roberto Reale <roberto.reale82@gmail.com>
2015-11-18 02:25:50 +0100

    Added two submodules under oracle/undo.

---
+++ oracle/undo/undo_actual_size.sql
@@ -0,0 +1,50 @@
+--------------------------------------------------------------------------------
+--
+--  The SQL Diaries
+-- 
+--  Philum:    Oracle
+--  Module:    undo
+--  Submodule: undo_actual_size
+--  Purpose:   shows actual undo size
+--  Reference: http://www.akadia.com/services/ora_optimize_undo.html
+--  Tested:    10g, 11g
+--
+--  Copyright (c) 2014-5 Roberto Reale
+--  
+--  Permission is hereby granted, free of charge, to any person obtaining a
+--  copy of this software and associated documentation files (the "Software"),
+--  to deal in the Software without restriction, including without limitation
+--  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+--  and/or sell copies of the Software, and to permit persons to whom the
+--  Software is furnished to do so, subject to the following conditions:
+--  
+--  The above copyright notice and this permission notice shall be included in
+--  all copies or substantial portions of the Software.
+--  
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+--  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+--  DEALINGS IN THE SOFTWARE.
+-- 
+--------------------------------------------------------------------------------
+
+
+CLEAR COLUMN
+COL undo_size  HEADING "Actual Undo Size (MiB)"
+
+
+SELECT
+    SUM(v$df.bytes) / 1048576  AS undo_size
+FROM
+    v$datafile   v$df
+JOIN
+    v$tablespace v$ts  ON v$df.ts# = v$ts.ts#
+JOIN
+    dba_tablespaces ts ON v$ts.name = ts.tablespace_name
+WHERE
+    ts.contents = 'UNDO' AND ts.status = 'ONLINE';
+
+--  ex: ts=4 sw=4 et filetype=sql
