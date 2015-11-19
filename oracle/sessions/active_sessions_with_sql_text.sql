75f10c2a30a44bc503b0ccae4130aa551b329ee9
Roberto Reale <roberto.reale82@gmail.com>
2015-11-19 14:12:45 +0100

    Reviewed submodule in oracle/sessions.

--- oracle/sessions/active_sessions_with_sql_text.sql
+++ oracle/sessions/active_sessions_with_sql_text.sql
@@ -1,20 +1,60 @@
+--------------------------------------------------------------------------------
 --
---  Active sessions with SQL text
+--  The SQL Diaries
+-- 
+--  Philum:    Oracle
+--  Module:    sessions
+--  Submodule: sess_active_sessions_with_sql
+--  Purpose:   shows active sessions with SQL text
+--  Tested:    10g, 11g
 --
---  Tested on: 10g
---
+--  Copyright (c) 2014-5 Roberto Reale
+--  
+--  Permission is hereby granted, free of charge, to any person obtaining a
+--  copy of this software and associated documentation files (the "Software"),
+--  to deal in the Software without restriction, including without limitation
+--  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+--  and/or sell copies of the Software, and to permit persons to whom the
+--  Software is furnished to do so, subject to the following conditions:
+--  
+--  The above copyright notice and this permission notice shall be included in
+--  all copies or substantial portions of the Software.
+--  
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+--  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+--  DEALINGS IN THE SOFTWARE.
+-- 
+--------------------------------------------------------------------------------
+
+CLEAR COLUMN
+SET LINE 200
+
+COL sql_text FORMAT a80 WORD_WRAPPED
 
 
-SELECT S.SID, S.SERIAL#, A.SQL_TEXT, S.USERNAME, S.MACHINE
-	FROM V$SQLTEXT A,
-	V$SESSION S
-	WHERE S.SQL_ADDRESS = A.ADDRESS
-	AND S.SID IN 
-(
-	SELECT SID
-	FROM V$SESSION
-	WHERE LAST_CALL_ET > 5
-	AND STATUS = 'ACTIVE'
-	AND USERNAME = '&USER'
-)
-ORDER BY S.SID, A.PIECE;
+SELECT
+    s.sid,
+    s.serial#,
+    sql.sql_text,
+    s.username,
+    s.machine
+FROM
+    v$sqltext sql JOIN v$session s ON s.sql_address = sql.address
+WHERE
+    s.sid IN
+        (
+            SELECT
+                sid
+            FROM
+                v$session
+            WHERE
+                last_call_et > 5 AND status = 'ACTIVE'
+        )
+ORDER BY
+    s.sid, sql.piece;
+
+--  ex: ts=4 sw=4 et filetype=sql
