a10902d42d4e64872efdf27b7429b10a02849c7e
Roberto Reale <roberto.reale82@gmail.com>
2015-11-19 14:34:47 +0100

    Reviewed submodule in oracle/dataguard.

--- oracle/dataguard/gap_analysis.sql
+++ oracle/dataguard/gap_analysis.sql
@@ -1,28 +1,64 @@
+--------------------------------------------------------------------------------
 --
---   Average REDO log applied for GAP analysis
+--  The SQL Diaries
+-- 
+--  Philum:    Oracle
+--  Module:    logs
+--  Submodule: log_count_and_average_size
+--  Purpose:   computes count of archived logs and their average size
+--  Tested:    10g, 11g
 --
---   @tested on: 10g
---
+--  Copyright (c) 2014-5 Roberto Reale
+--  
+--  Permission is hereby granted, free of charge, to any person obtaining a
+--  copy of this software and associated documentation files (the "Software"),
+--  to deal in the Software without restriction, including without limitation
+--  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+--  and/or sell copies of the Software, and to permit persons to whom the
+--  Software is furnished to do so, subject to the following conditions:
+--  
+--  The above copyright notice and this permission notice shall be included in
+--  all copies or substantial portions of the Software.
+--  
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+--  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+--  DEALINGS IN THE SOFTWARE.
+-- 
+--------------------------------------------------------------------------------
+
+CLEAR COLUMN
+SET LINE 200
 
 
+SELECT
+    lh.*,
+    lg.*,
+    ROUND(lh.archived# * lg.avg_mib)             AS daily_avg_mib
+FROM
+    (
+        SELECT
+            TO_CHAR(first_time, 'YYYY-MM-DD')    AS day,
+            COUNT(1)                             AS archived#,
+            MIN(recid)                           AS min#,
+            MAX(recid)                           AS max# 
+        FROM
+            gv$log_history
+        GROUP BY
+            TO_CHAR(first_time, 'YYYY-MM-DD')
+        ORDER BY 1 DESC
+    ) lh,
+    (
+        SELECT
+            COUNT(1)                             AS members#,
+            MAX(bytes) / 1048576                 AS max_mib, 
+            MIN(bytes) / 1048576                 AS min_mib,
+            AVG(bytes) / 1048576                 AS avg_mib
+        FROM
+            gv$log
+    ) lg;
 
-set line 200
-set PAGESIZE 9999
-col day format a15
-SELECT A.*, Round(A.Count#*B.AVG#/1024/1024) Daily_Avg_Mb FROM
-(SELECT To_Char(First_Time,'YYYY-MM-DD') DAY,
-Count(1) Count#, Min(RECID) Min#, Max(RECID) Max# FROM gv$log_history GROUP BY To_Char(First_Time,'YYYY-MM-DD')
-ORDER BY 1 DESC) A,(SELECT Avg(BYTES) AVG#, Count(1) Count#, Max(BYTES) Max_Bytes, 
-Min(BYTES) Min_Bytes FROM gv$log ) B;
-
----
-Sample output:
-
-DAY                 COUNT#       MIN#       MAX# DAILY_AVG_MB
---------------- ---------- ---------- ---------- ------------
-2015-08-13             524      33153      33286       131000
-2015-08-12             632      32995      33157       158000
-2015-08-11             600      32845      32999       150000
-2015-08-10             540      32710      32849       135000
-....
-
+--  ex: ts=4 sw=4 et filetype=sql
