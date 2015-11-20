0672f78af81714c1e2a34d8af75809d7443b705a
Roberto Reale <roberto.reale82@gmail.com>
2015-11-20 04:36:28 +0100

    Removed reduntant code from a file in oracle/security.

--- oracle/security/user-role-privileges.sql
+++ oracle/security/user-role-privileges.sql
@@ -4,18 +4,6 @@
 --  Tested on: 11g
 --
 
-SELECT lpad(' ', 2*LEVEL) || granted_role "Roles and privileges" FROM
-    (
-        SELECT NULL grantee, username granted_role FROM dba_users WHERE username = upper('user')
-        UNION
-        SELECT grantee, granted_role FROM dba_role_privs
-        UNION
-        SELECT grantee, privilege FROM dba_sys_privs
-    )
-START WITH grantee IS NULL CONNECT BY grantee = PRIOR granted_role
-/
-
-
 --  System privileges for a user
 
 SELECT PRIVILEGE
