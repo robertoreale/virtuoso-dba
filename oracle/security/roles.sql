11fea808e32920632bef26b2a843eeba98edb221
Roberto Reale <roberto.reale82@gmail.com>
2015-11-20 04:45:39 +0100

    Removed reduntant code from a file in oracle/security.

--- oracle/security/roles.sql
+++ oracle/security/roles.sql
@@ -8,17 +8,3 @@ SELECT PRIVILEGE
   FROM dba_role_privs rp JOIN role_sys_privs rsp ON (rp.granted_role = rsp.role)
  WHERE rp.grantee = <theUser>
  ORDER BY 1;
- 
--- Direct grants to tables/views:
-
-SELECT owner, table_name, select_priv, insert_priv, delete_priv, update_priv, references_priv, alter_priv, index_priv 
-  FROM table_privileges
- WHERE grantee = <theUser>
- ORDER BY owner, table_name;
- 
--- Indirect grants to tables/views:
-
-SELECT DISTINCT owner, table_name, PRIVILEGE 
-  FROM dba_role_privs rp JOIN role_tab_privs rtp ON (rp.granted_role = rtp.role)
- WHERE rp.grantee = <theUser>
- ORDER BY owner, table_name;
