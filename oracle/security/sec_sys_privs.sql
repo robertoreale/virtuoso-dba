e1f86aa77c79e80085ff85f02408931132c6eb3f
Roberto Reale <roberto.reale82@gmail.com>
2015-11-20 05:07:40 +0100

    Renamed and reviewed submodule in oracle/security.

---
+++ oracle/security/sec_sys_privs.sql
@@ -0,0 +1,54 @@
+--------------------------------------------------------------------------------
+--
+--  The SQL Diaries
+-- 
+--  Phylum:    Oracle
+--  Module:    security
+--  Submodule: sec_sys_privs
+--  Purpose:   system privileges for each user
+--  Tested:    10g, 11g
+--
+--  Copyright (c) 2014-5 Roberto Reale
+--  
+--  Permission is hereby granted, free of charge, to any person obtaining a
+--  copy of this software and associated documentation files (the "Software"),
+--  to deal in the Software without restriction, including without limitation
+--  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+--  and/or sell copies of the Software, and to permit persons to whom the
+--  Software is furnished to do so, subject to the following conditions:
+--  
+--  The above copyright notice and this permission notice shall be included in
+--  all copies or substantial portions of the Software.
+--  
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+--  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+--  DEALINGS IN THE SOFTWARE.
+-- 
+--------------------------------------------------------------------------------
+
+CLEAR COLUMN
+BREAK ON grantee
+
+
+SELECT
+    grantee,
+    privilege,
+    admin_option
+FROM
+    dba_sys_privs
+UNION SELECT
+    rp.grantee,
+    rsp.privilege,
+    rsp.admin_option
+FROM
+    dba_role_privs rp
+JOIN
+    role_sys_privs rsp ON rp.granted_role = rsp.role
+ORDER BY
+    grantee;
+
+--  ex: ts=4 sw=4 et filetype=sql
