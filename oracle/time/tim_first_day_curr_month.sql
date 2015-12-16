f9c7a6f865d39e6a5c02b8621797a0b583357403
Roberto Reale <roberto.reale82@gmail.com>
2015-12-17 00:23:46 +0100

    New module oracle/time.

---
+++ oracle/time/tim_first_day_curr_month.sql
@@ -0,0 +1,44 @@
+--------------------------------------------------------------------------------
+--
+--  The SQL Diaries
+-- 
+--  Phylum:    Oracle
+--  Module:    time
+--  Submodule: tim_first_day_curr_month
+--  Purpose:   gets the first day of the month
+--  Reference: http://viralpatel.net/blogs/useful-oracle-queries/
+--  Tested:    10g, 11g
+--
+--  Copyright (c) 2014-5 Roberto Reale
+--  
+--  Permission is hereby granted, free of charge, to any person obtaining a
+--  copy of this software and associated documentation files (the "Software"),
+--  to deal in the Software without restriction, including without limitation
+--  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+--  and/or sell copies of the Software, and to permit persons to whom the
+--  Software is furnished to do so, subject to the following conditions:
+--  
+--  The above copyright notice and this permission notice shall be included in
+--  all copies or substantial portions of the Software.
+--  
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+--  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+--  DEALINGS IN THE SOFTWARE.
+-- 
+--------------------------------------------------------------------------------
+
+
+CLEAR COLUMN
+COL first_day_curr_month FORMAT a22 HEADING "First day of the month"
+
+
+SELECT
+    TRUNC(SYSDATE, 'MONTH') first_day_curr_month
+FROM
+    dual;
+
+--  ex: ts=4 sw=4 et filetype=sql
